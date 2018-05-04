27d2cc02887ced136fd04c2193c9fd4c6b6a85f3
amieraahza <amieraahza28@gmail.com>
2018-05-04 11:00:42 +0800

    tambah file baru untuk recommendedWeb

---
+++ Aplikasi/Papar/homeuser/search_item/list_web.php
@@ -0,0 +1,11 @@
+<?php $pautan = URL . 'sumber/rangka-dawai/homeuser/';
+\Aplikasi\Kitab\Sesi::init();
+//echo '<pre>'; print_r($_SESSION); echo '</pre>';
+$classTable[] = "table table-bordered table-striped";
+$classTable[] = "table table-bordered table-striped with-check";
+$classTable[] = "table table-bordered data-table";
+
+include 'file_header.php';
+include 'file_menubar.php';
+include 'papar_jadual3.php';
+include 'file_footer.php';
---
+++ Aplikasi/Papar/homeuser/search_item/papar_jadual3.php
@@ -0,0 +1,15 @@
+<?php 
+foreach ($this->senarai as $myTable => $row)
+{
+	if ( count($row)==0 )
+		echo '';
+	else
+	{
+?>
+<!-- Jadual <?php echo $myTable ?> ########################################### -->
+<?php include 'papar_jadual3.1.php'; ?>
+<!-- Jadual <?php echo $myTable ?> ########################################### -->
+<?php
+	} // if ( count($row)==0 )
+}
+?>
