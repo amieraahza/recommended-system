6f10fc4bbd56dd9a05a0d1ca7a47244c2f8f0b07
amieraahza <amieraahza28@gmail.com>
2018-05-04 10:59:43 +0800

    tambah link recommended

--- Aplikasi/Papar/homeuser/search_item/file_menubar.php
+++ Aplikasi/Papar/homeuser/search_item/file_menubar.php
@@ -11,6 +11,7 @@
       <span class="icon-bar"></span>
       </button>
       <a class="navbar-brand" href="<?php echo URL ?>/homeuser/">Home</a>
+      <a class="navbar-brand" href="<?php echo URL ?>/homeuser/recommendWeb/">Recommended</a>
       <a class="navbar-brand" href="<?php echo URL ?>homeuser/logout">
         <span class="glyphicon glyphicon-off"></span>Logout</a>
     </div>
