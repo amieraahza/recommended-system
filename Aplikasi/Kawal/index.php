432cf307474eb076d973cb529bf7e1e6f78320ea
amieraahza <amieraahza28@gmail.com>
2018-07-07 10:29:43 +0800

    delete function muar

--- Aplikasi/Kawal/index.php
+++ Aplikasi/Kawal/index.php
@@ -43,22 +43,6 @@ class Index extends \Aplikasi\Kitab\Kawal
 		echo '</pre>|';//*/
 	}
 #==========================================================================================
-	function muar()
-	{
-		# Set pemboleubah utama
-		$this->papar->Tajuk_Muka_Surat='Enjin';
-		$this->papar->senaraiIP = dpt_ip(); # dapatkan senarai IP yang dibenarkan
-		$this->papar->ip = $ip = $_SERVER['REMOTE_ADDR'];
-		$this->papar->ip2 = substr($ip,0,10);
-		$this->papar->hostname = gethostbyaddr($_SERVER['REMOTE_ADDR']);
-		$this->papar->server = $_SERVER['SERVER_NAME'];
-		$this->papar->tajuk = 'Login Untuk Muar';
-
-		# Pergi papar kandungan
-		//$this->semakPembolehubah(); # Semak data dulu
-		$this->paparKandungan($this->_folder, 'muar', $noInclude=0); # $noInclude=0
-	}
-
 	function login($user)
 	{
 		# Set pemboleubah utama
